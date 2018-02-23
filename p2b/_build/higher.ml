c9bcc548ca95f194c36a4c0ed9589158874ccecc
mdale <maxl.dale.11@gmail.com>
2018-02-22 19:32:18 -0500

    Completed High Order Functions

--- p2b/_build/higher.ml
+++ p2b/_build/higher.ml
@@ -10,13 +10,40 @@ let is_over_x x lst =
 
 let count_over_x x lst =
 	let lstOver = is_over_x x lst in
-	fold (fun a b -> if b then a+1 else a) 0 lstOver
+	fold (fun a b -> if b then (a + 1) else a) 0 lstOver
 ;;
 
-let mean lst = failwith "unimplemented"
+let mean lst = 
+	let size = float_of_int (length lst) in
+	if size = 0. then
+		raise (Invalid_argument "mean")
+	else begin
+		let sum = fold (fun a b -> a +. b) 0. lst in
+		let res = (sum /. size) in
+		res
+	end
+;;
 
-let pred_succ lst = failwith "unimplemented"
+let concat_lists lst = 
+	let inner_f i_a i_h = i_h :: i_a in
+	let outer_f o_a o_h = fold inner_f o_a o_h in
+	let rev_concat = fold outer_f [] lst in
+	rev rev_concat
+;;
 
-let bind f lst = failwith "unimplemented"
+let pred_succ lst =
+	let mapped = map (fun a -> [a-1;a;a+1]) lst in
+	concat_lists mapped
+;;
 
-let ap fns args = failwith "unimplemented"
+let bind f lst = 
+	let mapped = map (fun a -> f a) lst in
+	concat_lists mapped
+;;
+
+let ap fns args = 
+	let rev_args = rev args in
+	let folded = fold (fun a f -> (map f rev_args) :: a) [] fns in
+	let rev_ap = concat_lists folded in
+	rev rev_ap
+;;
