1d7a97d7ece3e24b711b8c2d9b4e5db4acc33e6e
Maxl Dale <maxl.dale.11@gmail.com>
2018-04-03 22:43:24 +0200

    Completed outline for lexer

--- p3a/lexer.ml
+++ p3a/lexer.ml
@@ -1,3 +1,19 @@
 open SmallCTypes
 
-let tokenize input = failwith "Unimplemented"
+let str_to_tok tok = failwith "not implemented";;
+
+let tokenize_str_list str_list =
+	match str_list with
+	  [] -> []
+	| str :: tl_list -> (str_to_tok str) :: (tokenize_str_list tl_list)
+;;
+
+let split_on_whitespace str =
+	let white_space_r = (Str.regexp "[ \n\r\t]+") in
+	Str.split white_space_r str
+;;
+
+let tokenize input =
+	let split_input = (split_on_whitespace input) in
+	tokenize_str_list split_input
+;;
